2b57c49fb6bc3d52d32ea967d57739d5c8ced5f9
Farel-Jeheo <farelinojeheo@gmail.com>
2024-11-23 23:33:20 +0700

    Add files via upload

---
+++ MatematikaCanggih.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package matematikaa;
+
+public class MatematikaCanggih extends Matematika {
+    // Method untuk menghitung modulus
+    public int modulus(int a, int b) {
+        return a % b;
+    }
+}
+
