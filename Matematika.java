a44507103af6cea7946f9a25939d48b00290d4cc
Farel-Jeheo <farelinojeheo@gmail.com>
2024-11-23 23:16:55 +0700

    Add files via upload

---
+++ Matematika.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package matematikaa;
+
+public class Matematika {
+    // Method untuk pertambahan
+    public int pertambahan(int a, int b) {
+        return a + b;
+    }
+
+    // Method untuk perkalian
+    public int perkalian(int a, int b) {
+        return a * b;
+    }
+}
